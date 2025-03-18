b47327b635be4f0bd6c63afa5fa7a2afb11f8380
endang-ismaya <endang.ismaya@gmail.com>
2025-03-18 16:14:18 +0700

    add review model

--- apps/movies/admin.py
+++ apps/movies/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from apps.movies.models import Movie
+from apps.movies.models import Movie, Review
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     ordering = ["name"]
     search_fields = ["name", "description"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
