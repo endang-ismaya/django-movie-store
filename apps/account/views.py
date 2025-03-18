cef834b7e79d0fb231aac8b1297d5d04ae1eeace
endang-ismaya <endang.ismaya@gmail.com>
2025-03-18 16:08:04 +0700

    add login and logout functionality

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from apps.account.forms import CustomErrorList, CustomUserCreationForm
@@ -13,9 +17,39 @@ def signup(request):
         form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
-            return redirect("home.index")
+            return redirect("accounts.login")
         else:
             template_data["form"] = form
             return render(
                 request, "account/signup.html", {"template_data": template_data}
             )
+
+
+def login(request):
+    template_data = {}
+    template_data["title"] = "Login"
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect("home.index")
+
+        return render(request, "account/login.html", {"template_data": template_data})
+    elif request.method == "POST":
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user is not None:
+            auth_login(request, user)
+            return redirect("home.index")
+        else:
+            template_data["error"] = "The username or password is incorrect."
+            return render(
+                request, "account/login.html", {"template_data": template_data}
+            )
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect("home.index")
